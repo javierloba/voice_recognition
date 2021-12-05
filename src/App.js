5ef38f666ff6f6eefe4bc7c9a7eb02f8fb6bab7e
Javier Loba <30801844+javierloba@users.noreply.github.com>
2021-12-05 19:27:31 +0100

    Added styles & DisplayNotes component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css'
 import micon from "./img/mic_on.png"
 import micoff from "./img/mic_off.png"
-import { IconButton } from "@material-ui/core"
+import { IconButton, Typography } from "@material-ui/core"
 import { NeonButton } from "./components/StyledComponents"
+import DisplayNotes from './components/DisplayNotes'
 
 function App() {
   return (
@@ -25,6 +26,10 @@ function App() {
             <NeonButton status="done">
                 Done
             </NeonButton>
+            <Typography variant="h4" component="h2" gutterBottom>
+                Texto procesado
+            </Typography>
+            <DisplayNotes />
         </div>
     </>
   );
---
+++ src/components/DisplayNotes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayNotes = () => {
+    return (
+        <div>
+            <h1>estas son las notas</h1>
+        </div>
+    )
+}
+
+export default DisplayNotes
