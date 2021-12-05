3fe803002789d447e00c619076196b8ea0e4d223
Javier Loba <30801844+javierloba@users.noreply.github.com>
2021-12-05 21:52:55 +0100

    Created grid for notes with MUI

--- src/components/DisplayNotes.js
+++ src/components/DisplayNotes.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import { Grid, Paper, makeStyles } from '@material-ui/core';
+import uuid from "react-uuid"
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 
 const DisplayNotes = ({data}) => {
+    const classes = useStyles();
+
     return (
-        <div>
-        
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                {
+                    data.map((savedNote => (
+                        <Grid item xs={12} sm={4} key={savedNote.group}>
+                            {savedNote.name.map((string) => (
+                                <Paper className={classes.paper}>
+                                    <div key={uuid()}>{string}</div>
+                                </Paper>
+                            ))}
+                        </Grid>
+                    )))
+                }
+            </Grid>
         </div>
     )
 }
 
-export default DisplayNotes
+export default DisplayNotes;
