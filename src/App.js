4e25c5d80d0473172b435653b79048fa9003d075
Javier Loba <30801844+javierloba@users.noreply.github.com>
2021-12-05 12:50:55 +0100

    Created microphone button & basic styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import './App.css'
+import micon from "./img/mic_on.png"
+import micoff from "./img/mic_off.png"
+import { IconButton } from "@material-ui/core"
 
 function App() {
   return (
-    <div className="App">
-        <h1>Voice notes</h1>
-    </div>
+    <>
+        <div className="notes">
+            <h1>Voice notes</h1>
+            <div className="microphone">
+                <IconButton>
+                    <img className="mic-icon" src={micoff} alt="microphone" />
+                </IconButton>
+            </div>
+        </div>
+    </>
   );
 }
 
