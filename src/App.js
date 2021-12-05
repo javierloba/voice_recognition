a42641bbc3599a8e8fc81a6b3e918bbb7e0105a3
Javier Loba <30801844+javierloba@users.noreply.github.com>
2021-12-05 19:14:15 +0100

    Created buttons for todo, in process & done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import micon from "./img/mic_on.png"
 import micoff from "./img/mic_off.png"
 import { IconButton } from "@material-ui/core"
+import { NeonButton } from "./components/StyledComponents"
 
 function App() {
   return (
@@ -9,10 +10,21 @@ function App() {
         <div className="notes">
             <h1>Voice notes</h1>
             <div className="microphone">
+                {/* Este lo sacamos de material-ui */}
                 <IconButton>
                     <img className="mic-icon" src={micoff} alt="microphone" />
                 </IconButton>
             </div>
+            {/* Este lo creamos con styled components */}
+            <NeonButton status="todo">
+                To do
+            </NeonButton>
+            <NeonButton status="inprocess">
+                In process
+            </NeonButton>
+            <NeonButton status="done">
+                Done
+            </NeonButton>
         </div>
     </>
   );
--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -1,3 +1,33 @@
 import styled from "styled-components";
 
-export const Button = styled.button``
+export const MyButton = styled.button`
+    text-align: center;
+    color: #000;
+    text-transform: uppercase;
+    font-weight: 600;
+    cursor: pointer;
+    display: inline-block;
+`
+// Ahora creamos un botón de neon extendiendo
+// las propiedades de MyButton
+export const NeonButton = styled(MyButton)`
+    width: 130px;
+    padding: 10px 0;
+    margin-right: 10px;
+    background-color: transparent;
+    border: 3px solid ${({status})  => 
+    status === "done" ? "green" 
+    : status === "inprocess" ? "orange" : "crimson"};
+    border-radius: 50px;
+    color: ${({status})  => 
+    status === "done" ? "green" 
+    : status === "inprocess" ? "orange" : "crimson"};
+    &:hover {
+        box-shadow: 0 0 10px 0 ${({status})  => 
+        status === "done" ? "green" 
+        : status === "inprocess" ? "orange" : "crimson"} 
+        inset, 0 0 20px 2px ${({status})  => 
+        status === "done" ? "green" 
+        : status === "inprocess" ? "orange" : "crimson"};
+    }
+`
